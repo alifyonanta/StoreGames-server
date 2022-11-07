0fec47383a2fc68fc3bf635a74ff39206313d33a
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2022-11-07 08:50:55 +0700

    Versi 1.6.5
    - API detail history transaksi
    - API dashboard
    - API read & update profile

--- app/player/controller.js
+++ app/player/controller.js
@@ -5,6 +5,9 @@ const Nominal = require('../nominal/model')
 const Bank = require('../bank/model')
 const Payment = require('../payment/model')
 const Transaction = require('../transaction/model')
+const path = require('path')
+const fs = require('fs')
+const config = require('../../config')
 
 module.exports = {
     landingPage: async(req, res) => {
@@ -125,6 +128,119 @@ module.exports = {
                 data: history,
                 total : total.length ? total[0].value : 0
             })
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
+    },
+
+    historyDetail : async(req, res) => {
+        try {
+            const { id } = req.params
+            const { history } = await Transaction.findOne({_id : id})
+            if(!history) return res.status(404).json({message : "history tidak ditemukan."})
+            res.status(200).json({data : history})
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
+    },
+
+    dashboard : async(req, res) => {
+        try {
+            const count = await Transaction.aggregate([
+                { $match: { player: req.player._id}},
+                {
+                    $group: {
+                        _id: '$category',
+                        value: { $sum: '$value'}
+                    }
+                }
+            ])
+            const category = await Category.find({})
+            category.forEach(element => {
+                count.forEach(data => {
+                    if(data._id.toString() === element._id.toString()){
+                        data.name = element.name
+                    }
+                })
+            });
+            const history = await Transaction.find({player: req.player._id})
+            .populate('category')
+            .sort({'updateAt': -1})
+
+            res.status(200).json({data: history, count: count})
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
+    },
+
+    profile : async(req, res) => {
+        try {
+            const player = {
+                id: req.player._id,
+                username: req.player.username,
+                email: req.player.email,
+                name: req.player.name,
+                avatar: req.player.avatar,
+                phone_number: req.player.phoneNumber
+            }
+            res.status(200).json({data: player})
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
+    },
+
+    editProfile: async(req, res, next) => {
+        try {
+            const { name = "", phoneNumber= ""} = req.body
+            const payload = {}
+            if(name.length) payload.name = name
+            if(phoneNumber.length) payload.phoneNumber = phoneNumber
+            if(req.file){
+                let tmp_path = req.file.path;
+                //let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+                let filename = req.file.originalname;
+                let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+
+                const src = fs.createReadStream(tmp_path)
+                const dest = fs.createWriteStream(target_path)
+
+                src.pipe(dest)
+                src.on('end', async ()=> {
+                        let player = await Player.findOne({_id: req.player._id})
+                        let currentImage = `${config.rootPath}/public/uploads/${player.thumbnail}`;
+
+                        if(fs.existsSync(currentImage)){
+                            fs.unlinkSync(currentImage)
+                        }
+
+                        player = await Player.findOneAndUpdate({
+                            _id : req.player._id
+                        },{
+                            ...payload,
+                            avatar: filename
+                        }, {new: true, runValidators:true})
+                        res.status(201).json({
+                            data: {
+                                id: player.id,
+                                name: player.name,
+                                phoneNumber: player.phoneNumber,
+                                avatar: player.avatar
+                            }
+                        })
+                })
+            }else{
+                const player = await Player.findOneAndUpdate({
+                    _id: req.player._id
+                }, payload, {new: true, runValidators:true})
+                res.status(200).json({
+                    data:{
+                        id: player.id,
+                        name: player.name,
+                        phoneNumber: player.phoneNumber,
+                        avatar: player.avatar
+                    }
+                })
+            }
         } catch (err) {
             res.status(500).json({message: err.message || `Internal Server Error`})
         }
